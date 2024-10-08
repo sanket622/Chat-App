2cb4c04eb73b07fe893cdb5fa78964dbcdfd8def
sanket622 <sanketkumar0068@gmail.com>
2024-10-08 13:52:25 +0530

    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-chat-engine-pretty': 'react-chat-engine-pretty/dist/index.js', 
-    },
-  },
   optimizeDeps: {
-    include: ['react-chat-engine-pretty'],  
+    include: ['react-chat-engine-pretty'],
   },
   build: {
-    target: 'esnext',  
+    target: 'esnext',
     commonjsOptions: {
       transformMixedEsModules: true,
     },
