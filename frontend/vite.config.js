714de8ff55b02409181af162968b2f9d9e911365
sanket622 <sanketkumar0068@gmail.com>
2024-10-08 09:51:45 +0530

    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['react-chat-engine-pretty'], // Include the package for optimization
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-chat-engine-pretty'],  // Mark it as external to prevent bundling
+    },
+  },
+});
