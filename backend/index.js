80289119280a7df673d660f89931b91740234ae6
sanket622 <sanketkumar0068@gmail.com>
2024-10-09 18:48:04 +0530

    Update

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,6 @@ import dotenv from 'dotenv'
 const app = express()
 dotenv.config()
 app.use(express.json())
-// app.use(cors({
-//     origin: 'https://chatkro-three.vercel.app',  
-//     methods: 'GET,POST,PUT,DELETE',
-//     credentials: true, 
-//   }));
 app.use(cors());
 
 const CHAT_ENGINE_PROJECT_ID = process.env. CHAT_ENGINE_PROJECT_ID
